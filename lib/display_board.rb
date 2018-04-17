615e5b1e86c283a318832abdf197478f590ee820
Sara Tibbetts <sxt4866@rit.edu>
2018-04-17 00:32:08 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,4 +2,6 @@ def display_board(board)
   # board = [" ", " ", " ", ... ]
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  
 end
